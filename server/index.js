4a132bdacc1f0761ebfe7618e0046c8a795def79
designeng <denissavenok@gmail.com>
2015-12-22 20:47:03 +0300

    socket.io installed; server wrapper

--- server/index.js
+++ server/index.js
@@ -3,7 +3,10 @@ var app = require('./app');
 app.set('port', process.env.PORT || 8080);
 var port = app.get('port');
 
-app.listen(port, err => {
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(port, err => {
     if (err) {
         console.error(err);
         return;
