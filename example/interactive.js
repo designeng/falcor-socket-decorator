59a7cca321dda9f2882b08f6c139ba2984b05d95
designeng <denissavenok@gmail.com>
2015-12-25 16:06:26 +0300

    connectModel & callbackRegister together

--- example/interactive.js
+++ example/interactive.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import callbackRegister from "react-callback-register"
+import connectModel from 'connect-data-decorator';
 
+// connectModel & callbackRegister together
+@connectModel({
+    sourcePath  : '/users/model.json',
+    getValue    : 'users'
+})
 @callbackRegister
 export default class Interactive extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {users: []};
+    }
 
     @callbackRegister.on('mouseDown')
     activate(e) {
@@ -10,12 +21,21 @@ export default class Interactive extends React.Component {
     }
 
     render() {
+        let users = this.state.users;
         const {activate, ...other} = this.props;
+        
+        users = Object.keys(users).map(idx => {
+            return <li key={ idx }>
+                <a href={ "#" + users[idx].href } >
+                    { users[idx].name }
+                </a></li>;
+        });
 
         return (
-            <div {...other} {...this.callbacks}>
-                Interactive
+            <div>
+                <div {...other} {...this.callbacks}>User List</div>
+                <ul>{ users }</ul>
             </div>
-        )
+        );
     }
 }
